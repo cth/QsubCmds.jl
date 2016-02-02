eeb6329ca1a0f32b0d3c9527c4293c454aefe5ec
Christian Theil Have <c.have@sund.ku.dk>
2016-02-02 16:13:51 +0100

    add code+readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# ExternalQueueSubmit
+### ExtQueueSubmit.jl
+
+ExtQueueSubmit.jl is package for Julia intented to make it easy to submit 
+"shell scripts" represented as julia external commands, i.e., shell commands
+in backticks.  
+
+Currently, only Sun Grid Engine is support, but in the future other cluster
+managements systems may be supported.
+
+This package differs from ClusterManagers.jl in design. It is only intended 
+to submit "shell script" commands to the cluster and does support native
+Julia constructs. It works by translating one or more external commands
+to a shell script suitable for the grid engine to run. It then launches
+this script through the queue submission program, i.e., `qsub`. This has
+the advantage that it not necessary to have Julia available on cluster
+worker nodes. 
+
+
+### Example usage:
+
+Considering the [pipeline example from the Julia manual](http://docs.julialang.org/en/release-0.4/manual/running-external-programs/#pipelines)
+
+```julia
+myjob=pipeline(`do_work`, stdout=pipeline(`sort`, "out.txt"), stderr="errs.txt")
+```
+
+This can be submitted run as a cluster job using to using:
+
+```julia
+qsub(myjob)
+```
+
+This will immediately return the job-id. To wait for the job to finish, use `qwait`, e.g., 
+
+```julia
+qwait(myjob)
+```
+
--- src/ExternalQueueSubmit.jl
+++ src/ExternalQueueSubmit.jl
@@ -1,5 +1,64 @@
-module ExternalQueueSubmit
+# Submission of external commands via Sun Grid Engine 
+# Christian Theil Have, 2016.
 
-# package code goes here
+module ExtQueueSubmit
+	export qsub, qwait
+	import Base.Cmd,Base.OrCmds,Base.AndCmds,Base.CmdRedirect
 
-end # module
+	macro R_str(s)
+	    s
+	end
+
+	# Conversion of `external` commands in backticks to shell runable commands
+	to_shell(cmd::Cmd) = join(cmd.exec," ")
+	to_shell(cmd::OrCmds) = string(to_shell(cmd.a), " | ", to_shell(cmd.b)) 
+	to_shell(cmd::AndCmds) = string(to_shell(cmd.a), " & ", to_shell(cmd.b), " & ") 
+	to_shell(cmd::CmdRedirect) = string(to_shell(cmd.cmd), " ", cmd.stream_no, "> ", cmd.handle.filename)  
+
+	# Very rudimentary qsub functionality
+
+	function qsub(commands::Array{Union{Cmd,OrCmds,AndCmds,CmdRedirect}}, directory=(), depend="", options=[]) 
+		if depend != ""
+			depend = "-hold_jid $depend "
+		end
+
+		if directory == ()
+			directory = pwd()
+		end
+		
+		julia_executable=readlink("/proc/self/exe") # FIXME: Only works on linux
+
+		# Create a script
+		script=tempname()
+		open(script,"w") do file
+			write(file,string(R"#$ ","-S /bin/bash\n"))
+			write(file,string(R"#$ -cwd","\n"))
+			write(file,map(x -> string(R"#$ ", x), options))
+			write(file,map(x -> string(to_shell(x),"\n"),commands))
+		end 
+
+		# Run script and get JID
+		current_directory=pwd()
+		println(directory)
+		cd(directory)
+		output=readall(`qsub $script`)
+		cd(current_directory)
+
+		rx=r"Your job ([0-9]+) .* has been submitted"
+		if ismatch(rx, output)
+			match(rx,output)[1]
+		end
+	end
+
+	# Wait for job to terminate
+	qwait(jid) = try
+		while true
+			# This return 1 if process does not exists (e.g. if finished)
+			# in which case run throw a ProcessExited(1) exception
+			run(`qstat -j $jid`) 
+		end
+	catch 
+	end
+
+	qsub(cmd::Union{Cmd,OrCmds,AndCmds,CmdRedirect}, rest...) = qsub([cmd],rest)
+end
