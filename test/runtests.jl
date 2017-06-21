4b7f5714faf706fe22a8c0ff0f15dedd2978c67b
cth <christiantheilhave@gmail.com>
2017-06-21 23:15:24 +0200

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,23 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+
+macro with_qsub_env(expr::Expr)
+    mktempdir() do tmpdir
+        cd(tmpdir) do
+            info("running tests in: $(pwd())")
+            open("qsub", "w") do w
+                write(w,"echo Your job 123 blah has been submitted\n")
+            end
+            chmod("qsub",755)
+            withenv("PATH" => tmpdir) do  
+                eval(expr)
+            end
+        end
+    end
+end
+
+@with_qsub_env @test typeof(qsub(`test`)) == QsubCmds.Job 
+
+
